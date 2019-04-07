395fc05eba25edc98ecb8da01bc116c56e680fcb
Trisha <Trisha@Trisha-PC>
2019-04-07 21:15:43 +0530

    Completed successfully.

--- src/main/java/main/TicTacToe.java
+++ src/main/java/main/TicTacToe.java
@@ -6,6 +6,30 @@ public class TicTacToe{
 public static int[][] ticbox;
 	public static int win(int [][]tic) {
 		//check all rows
+		int sum1=0;
+		int sum2=0;
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<3;j++)
+			{
+				if(tic[i][j] == 1)
+				{
+					sum1++;
+					if(sum1==3)
+					{
+						return 1;
+					}
+				}
+				if(tic[i][j]==2)
+				{
+					sum2++;
+					if(sum2==3)
+					{
+						return 2;
+					}
+				}
+			}
+		}
 		//loop through rows from 0 to 3 and check if all the 3 places have same marks
  		
 		
@@ -13,6 +37,14 @@ public static int[][] ticbox;
 		//loop through columns from 0 to 3 and check if all the 3 places have same marks
  		
 		//check both diagonals 
+		if((tic[0][0]==1)&&(tic[1][1]==1)&&(tic[2][2]==1))
+		{
+			return 1;
+		}
+		if((tic[0][0]==2) && (tic[1][1]==2)&&(tic[2][2]==2))
+		{
+			return 2;
+		}
 		
 		
 		
@@ -34,14 +66,23 @@ public static int[][] ticbox;
 	}
 	public static boolean validate(int a1,int a2,int [][] tic) {
 		//check if a1 is between 0 & 3
+		int b=0;
+		
+		if((a1>=0) && (a1<3) && (a2>=0) && (a2<3)&& (tic[a1][a2]==0))
+			
+		{
+		 b=1;
+		}
 		//check if a2 is between 0 & 3
 		//check if the selected box is empty ie, already not marked by other player
-		//if all checks passed return true.
+		//if all checks passed 
+				
 		
-		
-		//Write your code here !!!
-		
-		
+		if(b==1)
+		{
+			return true;
+		}
+	
 		return false;
 	}
 	public static void main(String args[]) {
